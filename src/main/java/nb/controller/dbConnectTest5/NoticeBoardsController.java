593abe052bd42d608169f358a3c594ec4583e2cb
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-06-25 16:08:34 +0900

    select-option값과 검색창 questionVal값 확인

--- src/main/java/nb/controller/dbConnectTest5/NoticeBoardsController.java
+++ src/main/java/nb/controller/dbConnectTest5/NoticeBoardsController.java
@@ -10,5 +10,10 @@ public class NoticeBoardsController implements NbController{
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("NoticeBoardsContoller mvc2 신호");
 	
+		String field = request.getParameter("search");
+		System.out.println("field : "+field);
+	
+		String questionVal = request.getParameter("questionVal");
+		System.out.println("questionVal : "+questionVal);
 	}
 }
